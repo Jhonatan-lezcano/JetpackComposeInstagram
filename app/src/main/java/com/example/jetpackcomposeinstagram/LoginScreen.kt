ad5a32ac4d755c5fd3ce9b811091681ada845c59
jhonatan lezcano <jhonatanlezcano.plv@gmail.com>
2023-02-10 18:51:08 -0500

    sketch login

--- app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
@@ -1,6 +1,127 @@
 package com.example.jetpackcomposeinstagram
 
+import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
 
 @Composable
-fun LoginScreen() {}
+fun LoginScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Header(modifier = Modifier.align(Alignment.TopEnd))
+        Body(modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var isLoginEnable by rememberSaveable { mutableStateOf(false) }
+
+    Column(modifier = modifier) {
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email) { email = it }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password) { password = it }
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+        LoginButton(isLoginEnable)
+        Spacer(modifier = Modifier.size(16.dp))
+        LoginDivider()
+    }
+}
+
+@Composable
+fun LoginDivider() {
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+        Text(
+            text = "OR",
+            color = Color(0xFFB5B5B5),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 18.dp)
+        )
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean) {
+    Button(onClick = { /*TODO*/ }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Log In")
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot Password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF4EA8E9),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Password(value: String, onChange: (String) -> Unit) {
+    TextField(value = value, onValueChange = { onChange(it) }, modifier = Modifier.fillMaxWidth())
+}
+
+@Composable
+fun Email(value: String, onChange: (String) -> Unit) {
+    TextField(value = value, onValueChange = { onChange(it) }, modifier = Modifier.fillMaxWidth())
+}
+
+@Composable
+fun ImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "logo",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Header(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Close app",
+        modifier = modifier.clickable { activity.finish() })
+}
